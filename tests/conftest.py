e0877ae55f0e3060a05595a96a2f07e0b50c4cb5
Jess Friedersdorff <30495397+JessF298@users.noreply.github.com>
2026-09-24 11:08:35 +0100

    tests: figured out how to use temp factory for taxapleae, but not implemented, storing in case needed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,17 @@ import os
 os.environ["CONFIG"] = "Placeholder config"
 os.environ["ONYX_DOMAIN"] = "Placeholder domain"
 os.environ["ONYX_TOKEN"] = "Placeholder token"
+
+
+# def get_taxaplease_db(path_to_db):
+#     tp = TaxaPlease()
+#     tp = TaxaPlease(database=path_to_db)
+#     tp.set_taxonomy_url("https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/new_taxdump_2026-03-01.zip")
+
+
+# @pytest.fixture(scope="session")
+# def taxaplease_db(tmpdir_factory):
+
+#     path_to_db = Path(tmpdir_factory.mktemp("data").join("taxa.db"))
+#     get_taxaplease_db(path_to_db)
+#     return path_to_db
